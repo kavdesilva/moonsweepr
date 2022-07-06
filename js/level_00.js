d81e958d6bbcdb1aceffa7cb6590d69ff2eb3d18
kavdesilva <kavdesilva@gmail.com>
2022-07-06 11:32:33 -0400

    following 101computing tutorial; have the grid and generated and mines marked with x. added styling for grid with future mine placement according to instructions.

--- js/level_00.js
+++ js/level_00.js
@@ -1,6 +1,43 @@
-let returnHome = document.getElementById('return-home')
+///////////////////////////////////// return home key
+
+const returnHome = document.getElementById('return-home')
 
 returnHome.addEventListener('click', () => {
     window.location='intro.html'
 })
 
+///////////////////////////////////// build board **(101computing.net tutorial)**
+
+const grid = document.getElementById('grid')
+const testMode = true; // set to true to see mine location
+
+const generateGrid = () => {
+    // generates 10x10 grid
+    // i = row; j = column
+    grid.innerHTML = ""
+    for (i=0; i<10; i++){
+        row = grid.insertRow(i);
+        for (j=0; j<10; j++){
+            cell = row.insertCell(j);
+            cell.onclick = () => {clickCell(this)}
+            let mine = document.createAttribute('data-mine')
+            mine.value = 'true'
+            cell.setAttributeNode(mine)
+        }
+    }
+    addMines()
+}
+
+const addMines = () => {
+    // adds mines randomly
+    for (i=0; i<20; i++){
+        let row = Math.floor(Math.random() * 10)
+        let col = Math.floor(Math.random() * 10)
+        let cell = grid.rows[row].cells[col]
+        cell.setAttribute('data-mine', 'true')
+        if (testMode) cell.innerHTML='X'
+    }
+}
+generateGrid()
+
+///////////////////////////////////// game logic
