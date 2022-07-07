f5cb98a53cc1436d81fd3ca2d0724a6e3e55797a
kavdesilva <kavdesilva@gmail.com>
2022-07-07 15:26:53 -0400

    added values to mine adacent squares, but something is wrong with my recursive call to clear empty squares when a square with a value of 0 is clicked. will troubleshoot.

--- js/level_00.js
+++ js/level_00.js
@@ -18,7 +18,7 @@ const createBoard = () => {
         row = grid.insertRow(i);
         for (j=0; j<10; j++){
             cell = row.insertCell(j);
-            cell.onclick = function() { clickCell(this); } 
+            cell.onclick = function() {click(this);}
         }
     }
     for (i=0; i<20; i++){
@@ -30,10 +30,46 @@ const createBoard = () => {
 }
 createBoard()
 
-
-
 ///////////////////////////////////// game logic
 
+let gameOver = false
 
+const click = (cell) => {
+    if (gameOver === true) return
+    if (cell.classList.contains('mine')){
+        alert('game over')
+        gameOver = true
+        for (i=0; i<10; i++) {
+            for (j=0; j<10; j++){
+                let cell = grid.rows[i].cells[j]
+                if (cell.classList.contains('mine')) cell.style.backgroundColor = 'orangered'
+            }
+        }
+    } else {
+        cell.setAttribute('class', 'checked')
+        let mineCount = 0
+        let cellRow = cell.parentNode.rowIndex
+        let cellCol = cell.cellIndex
+        for (i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++){
+            for (j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++){
+                if (grid.rows[i].cells[j].classList.contains('mine')) mineCount++;
+            }
+        }
+        if (mineCount===0){
+            for (i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
+                for(j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
+                  if (grid.rows[i].cells[j].innerHTML=='') click(grid.rows[i].cells[j]);
+                }
+            }
+        }
+        if (mineCount !=0) {
+            if (mineCount == 1) cell.classList.add('one')
+            if (mineCount == 2) cell.classList.add('two')
+            if (mineCount == 3) cell.classList.add('three')
+            if (mineCount >= 4) cell.classList.add('four-plus')
+            cell.innerHTML = mineCount
+        }
+    }
+}
 
 ///////////////////////////////////// completed: n/a
